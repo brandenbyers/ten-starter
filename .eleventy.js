7ee11ee1b1072da34dd51b9e4aad2a30f62ca3e0
Branden Byers <branden@brandenbyers.com>
2020-04-30 23:32:40 -0700

    Switch name to TENS

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ const markdownItFootnote = require('markdown-it-footnote')
 const markdownItContainer = require('markdown-it-container')
 const markdownItMark = require('markdown-it-mark')
 const markdownItAnchor = require('markdown-it-github-headings')
-const customLinkIcon = `<svg fill="currentColor" viewBox="0 0 20 20"><path d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>`
+const customLinkIcon = `<svg fill="black" viewBox="0 0 20 20"><path d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>`
 
 module.exports = function (config) {
   // Plugins
@@ -26,25 +26,9 @@ module.exports = function (config) {
     linkIcon: customLinkIcon,
   }).use(markdownItEmoji)
   .use(markdownItAbbr)
-    // EXAMPLE:
-    // *[HTML]: Hyper Text Markup Language
-    // *[W3C]:  World Wide Web Consortium
-    // The HTML specification
-    // is maintained by the W3C.
-    // RESULT:
-    // <p>The <abbr title="Hyper Text Markup Language">HTML</abbr> specification
-    // is maintained by the <abbr title="World Wide Web Consortium">W3C</abbr>.</p>
   .use(markdownItFootnote)
   .use(markdownItContainer)
-    // EXAMPLE:
-    // ::: warning
-    // *here be dragons*
-    // :::
-    // RESULT:
-    // <div class="warning">
-    // <em>here be dragons</em>
-    // </div>
-  .use(markdownItMark) // md.render('==marked==') // => '<p><mark>marked</mark></p>'
+  .use(markdownItMark)
   config.setLibrary('md', mdlib)
 
   // Layouts
